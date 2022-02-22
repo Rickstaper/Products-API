bb16e6199c684dc3aec6f93feda25071c268b546
AlexanderKarcheuski <karchevskji@gmail.com>
2022-02-23 00:10:36 +0300

    Created data transfer object

---
+++ Products.Data/DataTransferObject/FridgeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Data.DataTransferObject
+{
+    public class FridgeDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string OwnerName { get; set; }
+        public Guid FridgeModelId { get; set; }
+    }
+}
