2788e3abd2ec2a24a702c3d8fae4d82ecb8a7764
AlexanderKarcheuski <karchevskji@gmail.com>
2022-02-23 00:10:01 +0300

    Created MappingProfile

---
+++ Products.API/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Products.Data.DataTransferObject;
+using Products.Data.Models;
+
+namespace Products_API.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Fridge, FridgeDto>();
+        }
+    }
+}
