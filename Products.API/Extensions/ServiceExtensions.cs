4ec8af7fb644077b6970548415d9e769ef92d866
AlexanderKarcheuski <karchevskji@gmail.com>
2022-02-23 11:56:40 +0300

    Added logger in console

--- Products.API/Extensions/ServiceExtensions.cs
+++ Products.API/Extensions/ServiceExtensions.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Products.Contracts;
 using Products.Data;
 using Products.Repository;
+using System;
 
 namespace Products_API.Extensions
 {
@@ -12,8 +14,8 @@ namespace Products_API.Extensions
         public static void ConfigureSqlContext(this IServiceCollection services,
             IConfiguration configuration) =>
             services.AddDbContext<ProductsContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b =>
-            b.MigrationsAssembly("Products.API")));
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("Products.API"))
+            .LogTo(Console.WriteLine, LogLevel.Information));
 
         public static void ConfigureRepositoryManager(this IServiceCollection services)
         {
